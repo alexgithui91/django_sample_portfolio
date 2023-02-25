b358fae1879117f419a6cdab40752228657df178
alexgithui91@gmail.com <alexgithui91@gmail.com>
2023-02-25 14:28:23 +0300

    able to send email from blog

--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ from django.core.mail import BadHeaderError
 from django.http import HttpResponse
 from django.core.mail import EmailMessage
 from django.conf import settings
+from .utils import send_inquiry_email
 import os
 
 
@@ -25,26 +26,6 @@ class HomeTemplateView(TemplateView):
         return context
 
     def post(self, request):
-        def send_inquiry_email(
-            request,
-            mail_subject,
-            email_template,
-            message,
-            full_name,
-            email,
-        ):
-            from_email = settings.EMAIL_HOST_USER
-            message = render_to_string(
-                email_template,
-                {"full_name": full_name, "message": message},
-            )
-
-            to_email = settings.EMAIL_HOST_USER
-            mail = EmailMessage(
-                mail_subject, message, from_email, to=[to_email]
-            )
-            mail.send()
-
         if request.method == "POST":
             message = request.POST["message"]
             full_name = request.POST["name"]
